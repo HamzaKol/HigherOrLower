6ed0b4bb5c4fe047bfc297484e29cb8eb7650061
HamzaKol <hamza.kolasinac13@gmail.com>
2024-06-07 20:01:44 +0200

    izbacivanje API ključa

--- HigherOrLower/app/src/main/java/com/example/higherorlower/network/HoLApiService.kt
+++ HigherOrLower/app/src/main/java/com/example/higherorlower/network/HoLApiService.kt
@@ -11,7 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://imdb-top-100-movies.p.rapidapi.com/"
-private const val API_KEY = "c72070a73amsh9fdaa062595d7bdp14cf39jsna76e12d5e314"
+private const val API_KEY = "" //Ovdje staviti API ključ da bi se mogla koristiti aplikacija
 private const val API_HOST = "imdb-top-100-movies.p.rapidapi.com"
 
 // Interceptor to add the API key to requests
