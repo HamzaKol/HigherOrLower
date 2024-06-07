4dc095b43b2497184c87baed83e3b91338ae826e
Amar Koric <amar.koric02@gmail.com>
2024-06-07 12:38:51 +0200

    Screens kategorije

--- HigherOrLower/app/src/main/java/com/example/higherorlower/ui/screens/CategoriesScreen.kt
+++ HigherOrLower/app/src/main/java/com/example/higherorlower/ui/screens/CategoriesScreen.kt
@@ -1,9 +1,20 @@
 package com.example.higherorlower.ui.screens
 
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
+fun CategoryCard() {
+    Card (
+
+    ) {
+
+    }
+}
+
+
+@Composable
 fun CategoriesScreen () {
-    Text(text = "Ovo je Categories Screen")
+
 }
